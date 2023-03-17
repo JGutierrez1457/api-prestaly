a3f12ec37fba0ecdbc94f8353a109c9ed5ee703f
JGutierrez1457 <jgutierrezch@uni.pe>
2023-03-16 20:50:22 -0500

    Camabiado por end

--- components/loans/loans.controller.js
+++ components/loans/loans.controller.js
@@ -105,8 +105,7 @@ loansController.getNoBalancedLoansPDF = async (req, res)=>{
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', 'attachment; filename=pre-balance.pdf');
         fileContent.pipe(res);
-        fileContent.close();
-        fileContent.on("close", function (){
+        fileContent.on("end", function (){
             fs.unlinkSync(pathFile);
         });
     } catch (error) {
